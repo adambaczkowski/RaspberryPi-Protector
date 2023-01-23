75deca70ba7ef62f67efb5c26479cc007ff30142
Adam Bączkowski <48407212+adambaczkowski@users.noreply.github.com>
2023-01-23 20:15:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,56 +54,56 @@ function main() {
     check_device_info
     sleep 2
     start_time=$(date +%s)
-    Update || echo -ne "${RED}Update has failed ❌${NO_COLOR}"
+    Update #|| echo -ne "${RED}Update has failed ❌${NO_COLOR}"
     sleep 2
-    snort || echo -ne "${RED}Snort installation has failed ❌${NO_COLOR}"
+    snort #|| echo -ne "${RED}Snort installation has failed ❌${NO_COLOR}"
     sleep 2
-    docker_installer || echo -ne "${RED}Dokcer installation has failed ❌${NO_COLOR}"
+    docker_installer #|| echo -ne "${RED}Dokcer installation has failed ❌${NO_COLOR}"
     sleep 2
-    yacht_installer || echo -ne "${RED}Yacht installation has failed ❌${NO_COLOR}"
+    yacht_installer #|| echo -ne "${RED}Yacht installation has failed ❌${NO_COLOR}"
     sleep 2
     sudo docker update --restart unless-stopped $(sudo docker ps -q)
     sleep 2
-    grafana_installer || echo -ne "${RED}Grafana installation has failed ❌${NO_COLOR}"
+    grafana_installer #|| echo -ne "${RED}Grafana installation has failed ❌${NO_COLOR}"
     sleep 2
-    prometheus_installer || echo -ne "${RED}Prometheus installation has failed ❌${NO_COLOR}"
+    prometheus_installer #|| echo -ne "${RED}Prometheus installation has failed ❌${NO_COLOR}"
     sleep 2
-    node_exporter_installer || echo -ne "${RED}Node Exporter installation has failed ❌${NO_COLOR}"
+    node_exporter_installer #|| echo -ne "${RED}Node Exporter installation has failed ❌${NO_COLOR}"
     sleep 2
-    loki_installer || echo -ne "${RED}Loki installation has failed ❌${NO_COLOR}"
+    loki_installer #|| echo -ne "${RED}Loki installation has failed ❌${NO_COLOR}"
     sleep 2
-    mail_setup || echo -ne "${RED}Setting up mail installation has failed ❌${NO_COLOR}"
+    mail_setup #|| echo -ne "${RED}Setting up mail installation has failed ❌${NO_COLOR}"
     sleep 2
-    promtail_installer || echo -ne "${RED}Promtail installation has failed ❌${NO_COLOR}"
+    promtail_installer #|| echo -ne "${RED}Promtail installation has failed ❌${NO_COLOR}"
     sleep 2
-    fail2ban_installer || echo -ne "${RED}Fail2ban installation has failed ❌${NO_COLOR}"
+    fail2ban_installer #|| echo -ne "${RED}Fail2ban installation has failed ❌${NO_COLOR}"
     sleep 2
-    ClamAV_installer || echo -ne "${RED}ClamAV installation has failed ❌${NO_COLOR}"
+    ClamAV_installer #|| echo -ne "${RED}ClamAV installation has failed ❌${NO_COLOR}"
     sleep 2
-    OS_Hardening || echo -ne "${RED}OS Hardening has failed ❌${NO_COLOR}"
+    OS_Hardening #|| echo -ne "${RED}OS Hardening has failed ❌${NO_COLOR}"
     sleep 2
-    Kernel_Hardening || echo -ne "${RED}Kernel hardening has failed ❌${NO_COLOR}"
+    Kernel_Hardening #|| echo -ne "${RED}Kernel hardening has failed ❌${NO_COLOR}"
     sleep 2
     sudo apt-get install aha -y
-    AuditD_installer|| echo -ne "${RED}AuditD installation has failed ❌${NO_COLOR}"
+    AuditD_installer #|| echo -ne "${RED}AuditD installation has failed ❌${NO_COLOR}"
     sleep 2
-    Rkhunter_installer|| echo -ne "${RED}Rkhunter installation has failed ❌${NO_COLOR}"
+    Rkhunter_installer #|| echo -ne "${RED}Rkhunter installation has failed ❌${NO_COLOR}"
     sleep 2
     #Honeypot_installer || echo -ne "${RED}Setting up Honeypot has failed ❌${NO_COLOR}"
     sleep 2
-    Lynis_installer || echo -ne "${RED}Lynis installation has failed ❌${NO_COLOR}"
+    Lynis_installer #|| echo -ne "${RED}Lynis installation has failed ❌${NO_COLOR}"
     sleep 2
-    Docker_Bench_Installer || echo -ne "${RED}Docker Bench installation has failed ❌${NO_COLOR}"
+    Docker_Bench_Installer #|| echo -ne "${RED}Docker Bench installation has failed ❌${NO_COLOR}"
     sleep 2
     Firewall
     sleep 2
     echo -ne q | crontab -e
     echo -ne "Setting up additional scripts 📜"
-    DDOS_Mail_Setup || echo -ne "${RED}Setting DDos mail script has failed ❌${NO_COLOR}"
+    DDOS_Mail_Setup #|| echo -ne "${RED}Setting DDos mail script has failed ❌${NO_COLOR}"
     sleep 2
-    High_RAM_Mail_Setup || echo -ne "${RED}Setting High RAM notification mail script has failed ❌${NO_COLOR}"
+    High_RAM_Mail_Setup #|| echo -ne "${RED}Setting High RAM notification mail script has failed ❌${NO_COLOR}"
     sleep 2
-    Cleanup || echo -ne "${RED}Cleanup function has failed ❌${NO_COLOR}"
+    Cleanup #|| echo -ne "${RED}Cleanup function has failed ❌${NO_COLOR}"
     summary
     
     end_time=$(date +%s)
@@ -117,7 +117,7 @@ function main() {
 function enable_colors(){
     sudo sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
     sudo echo -ne "export LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;'" | sudo tee -a ~/.bashrc
-    sudo source ~/.bashrc
+    source ~/.bashrc
 }
 
 function Update() {
